a1ababb663a12d9ef465e022f9115f2c696fa725
yboateng94 <ybdecipher@outlook.com>
2025-04-01 17:28:40 +0100

    Update urls.py

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('books/', views.list_books, name='list_books'),  # Function-based view
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+]
+
 """
 URL configuration for LibraryProject project.
 
