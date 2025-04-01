7a9b82e615ca8f434924221043acb77c6b33fa73
Yeboah Boateng <ybdecipher@outlook.com>
2025-04-01 17:11:55 +0100

    final

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -9,5 +9,4 @@ def list_books(request):
 
 # Class-based View to display details of a specific library
 class LibraryDetailView(DetailView):
-    model = Library
-    template_name = ["relationship_app/'library_detail.html", "from .models import library"]
+    model = Library ["relationship_app/'library_detail.html", "from .models import library"]
