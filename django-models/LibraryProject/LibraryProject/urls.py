5a85083f6d5511db657613071f01a376bb547e28
yboateng94 <ybdecipher@outlook.com>
2025-04-01 17:41:57 +0100

    Update urls.py

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -1,8 +1,7 @@
 
+from django.contrib import admin
 from django.urls import path
-from . import views
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),  # Function-based view
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+    path('admin/', admin.site.urls),
 ]
