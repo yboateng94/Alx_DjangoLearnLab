ad090c9448698011ce4d832d382a03c794e79eb1
Yeboah Boateng <ybdecipher@outlook.com>
2025-04-06 12:59:53 +0100

    api

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, BookList
+from .views import BookViewSet
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    # Route for the BookList view (optional, if you want to retain the previous list view)
     path('books/', BookList.as_view(), name='book-list'),
-
     # Include the router URLs for CRUD operations
     path('', include(router.urls)),
 ]
